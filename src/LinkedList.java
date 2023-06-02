d8e7dd6360bbf37237f632e578859267da618b23
Tretyakov Ivan <timmitarh@mail.ru>
2023-06-02 09:56:06 +0300

    Completed task

---
+++ src/LinkedList.java
@@ -0,0 +1,30 @@
+// Урок 3. Структуры данных. Связный список.
+//        Необходимо реализовать метод разворота связного списка (двухсвязного или односвязного на выбор).
+
+// Реализация метода разворота двухсвязного списка.
+public class LinkedList{
+    private Node head;
+    private Node tail;
+
+    class Node{
+        private int value;
+        private Node nextNode;
+        private Node previousNode;
+    }
+
+    public void revert(){
+        Node node = head;
+
+        Node temp = head;
+        head = tail;
+        tail = temp;
+
+        while (node.nextNode != null){
+            temp = node.nextNode;
+            node.nextNode = node.previousNode;
+            node.previousNode = temp;
+            node = node.previousNode;
+        }
+    }
+
+}
